730d63288d181f571788352406fcc67402214eab
Simon Emms <simon@simonemms.com>
2018-03-22 20:34:24 +0000

    Improved the logging for the updates

--- files/update.sh
+++ files/update.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+exec &> /var/log/update-cronjob
+
 set -e
+set -x
 
 echo "[$(date)]: Update cronjob started" >> /var/log/update-cronjob
 
@@ -11,3 +14,9 @@ apt-get autoremove -y
 apt-get autoclean -y
 
 echo "[$(date)]: Update cronjob finished" >> /var/log/update-cronjob
+
+# Check if we need to reboot the Pi
+if [ -f /var/run/reboot-required.pkgs ]; then
+  echo "[$(date)]: Rebooting after update cronjob" >> /var/log/update-cronjob
+  reboot
+fi
