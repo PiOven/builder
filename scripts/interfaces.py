f5e1f81c6e639e7ff91d55e7fdeb81bf81b49f9b
Simon Emms <simon@simonemms.com>
2018-04-05 18:33:06 +0100

    Added JS/Python linting - and linted them

--- scripts/interfaces.py
+++ scripts/interfaces.py
@@ -1,17 +1,19 @@
-import netifaces
+""" Sets up the interfaces for the Pi """
 import os
 import socket
 import struct
+import netifaces  # pylint: disable=import-error
 
 
 def decimal_to_ip(decimal):
+    """ Converts a decimal into an IP address """
     return socket.inet_ntoa(struct.pack('!L', decimal))
 
 
 def get_default_gateway_linux():
-    """Read the default gateway directly from /proc."""
-    with open("/proc/net/route") as fh:
-        for line in fh:
+    """ Read the default gateway directly from /proc. """
+    with open("/proc/net/route") as file_handler:
+        for line in file_handler:
             fields = line.strip().split()
             if fields[1] != "00000000" or not int(fields[3], 16) & 2:
                 continue
@@ -20,6 +22,7 @@ def get_default_gateway_linux():
 
 
 def get_ip_address(start_ip, end_ip):
+    """ Gets an unused IP address in the range """
     start_ip_decimal = ip_to_decimal(start_ip)
     end_ip_decimal = ip_to_decimal(end_ip)
     target_ip = None
@@ -44,55 +47,53 @@ def get_ip_address(start_ip, end_ip):
     return target_ip
 
 
-def ip_to_decimal(ip):
-    packed_ip = socket.inet_aton(ip)
+def ip_to_decimal(ip_address):
+    """ Converts an IP into decimal """
+    packed_ip = socket.inet_aton(ip_address)
     return struct.unpack("!L", packed_ip)[0]
 
 
-ifaces = [
+IFACES = [
     'eth0',
     'wlan0',
     'wlan1',
     'wlp2s0'
 ]
 
-values = []
+VALUES = []
 
-target_ip_address = None
-start_ip = os.environ.get('PI_IP_ADDRESS_RANGE_START')
-end_ip = os.environ.get('PI_IP_ADDRESS_RANGE_END')
-dns_address = os.environ.get('PI_DNS_ADDRESS')
+TARGET_IP_ADDRESS = None
+CONFIG_START_IP = os.environ.get('PI_IP_ADDRESS_RANGE_START')
+CONFIG_END_IP = os.environ.get('PI_IP_ADDRESS_RANGE_END')
+DNS_ADDRESS = os.environ.get('PI_DNS_ADDRESS')
 
-if start_ip is not None and end_ip is not None:
-    target_ip_address = get_ip_address(start_ip, end_ip)
+if CONFIG_START_IP is not None and CONFIG_END_IP is not None:
+    TARGET_IP_ADDRESS = get_ip_address(CONFIG_START_IP, CONFIG_END_IP)
 
-for iface in ifaces:
+for iface in IFACES:
     try:
         gateway = get_default_gateway_linux()
         addrs = netifaces.ifaddresses(iface)[netifaces.AF_INET]
 
-        if target_ip_address is not None:
+        if TARGET_IP_ADDRESS is not None:
             # We've got an IP address range set
-            addrs[0]['addr'] = target_ip_address
+            addrs[0]['addr'] = TARGET_IP_ADDRESS
 
-        values.append({
+        VALUES.append({
             "iface": iface,
             "values": [
                 addrs[0],
                 gateway
             ]
         })
-    except Exception as err:
-        """
-        Do nothing
-        """
+    except Exception as err:  # pylint: disable=broad-except
+        # Do nothing
+        print(err)
 
-if len(values) == 0:
+if len(VALUES) == 0:   # pylint: disable=len-as-condition
     raise Exception('No interfaces available')
 
-"""
-Do the output
-"""
+# Do the output
 with open(os.environ.get('NETWORK_CONFIG'), 'w') as the_file:
     the_file.write('# interfaces(5) file used by ifup(8) and ifdown(8)\n')
     the_file.write('\n')
@@ -106,20 +107,20 @@ with open(os.environ.get('NETWORK_CONFIG'), 'w') as the_file:
     the_file.write('iface lo inet loopback\n')
     the_file.write('\n')
 
-    for value in values:
+    for value in VALUES:
         iface = value['iface']
-        values = value['values']
+        VALUES = value['values']
 
         if iface.startswith('wlan'):
             the_file.write('allow-hotplug ' + iface + '\n')
 
-        if values:
+        if VALUES:
             the_file.write('iface ' + iface + ' inet static\n')
             the_file.write('    wireless-power off\n')
-            the_file.write('    address ' + values[0]['addr'] + '\n')
-            the_file.write('    netmask ' + values[0]['netmask'] + '\n')
-            the_file.write('    gateway ' + values[1] + '\n')
-            the_file.write('    gateway dns-nameservers ' + dns_address + '\n')
+            the_file.write('    address ' + VALUES[0]['addr'] + '\n')
+            the_file.write('    netmask ' + VALUES[0]['netmask'] + '\n')
+            the_file.write('    gateway ' + VALUES[1] + '\n')
+            the_file.write('    gateway dns-nameservers ' + DNS_ADDRESS + '\n')
         else:
             the_file.write('iface ' + iface + ' inet manual\n')
 
@@ -129,4 +130,4 @@ with open(os.environ.get('NETWORK_CONFIG'), 'w') as the_file:
         the_file.write('\n')
 
 with open(os.environ.get('TARGET_IP'), 'w') as the_file:
-    the_file.write(target_ip_address)
+    the_file.write(TARGET_IP_ADDRESS)
